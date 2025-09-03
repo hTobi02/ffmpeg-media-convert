72ba1593ada92de4a5ff8684b69a00db242f6fc1
hTobi02 <67925751+hTobi02@users.noreply.github.com>
2025-09-03 22:23:08 +0200

    change behavior for switches

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,27 +12,40 @@ add_param() {
   fi
 }
 
+add_switch() { # nur Flag setzen, wenn logisch "true"
+  v="$(printf '%s' "$2" | tr '[:upper:]' '[:lower:]')"
+  case "$v" in 1|true|yes|on) PARAMS="$PARAMS -$1" ;; esac
+}
+
 # Required
 add_param "SourcePath" "$SOURCE_PATH"
-add_param "DestPath" "$DEST_PATH"
+add_param "DestPath"   "$DEST_PATH"
 
-# Optional
-add_param "TargetFps" "$TARGET_FPS"
-add_param "AllowFpsUpsample" "$ALLOW_FPS_UPSAMPLE"
-add_param "VideoCodec" "$VIDEO_CODEC"
-add_param "Preset" "$PRESET"
+# Optional (normale Parameter)
+add_param "TargetFps"    "$TARGET_FPS"
+add_param "VideoCodec"   "$VIDEO_CODEC"
+add_param "Preset"       "$PRESET"
 add_param "VideoBitrate" "$VIDEO_BITRATE"
-add_param "MaxRate" "$MAXRATE"
-add_param "BufSize" "$BUFSIZE"
-add_param "CRF" "$CRF"
-add_param "AudioMode" "$AUDIO_MODE"
+add_param "MaxRate"      "$MAXRATE"
+add_param "BufSize"      "$BUFSIZE"
+add_param "CRF"          "$CRF"
+add_param "AudioMode"    "$AUDIO_MODE"
 add_param "AudioBitrate" "$AUDIO_BITRATE"
-add_param "MapSubtitles" "$MAP_SUBTITLES"
-add_param "Overwrite" "$OVERWRITE"
-add_param "DryRun" "$DRYRUN"
-add_param "Extensions" "$EXTENSIONS"
-add_param "Suffix" "$SUFFIX"
-add_param "Threads" "$THREADS"
+add_param "Extensions"   "$EXTENSIONS"
+add_param "Suffix"       "$SUFFIX"
+add_param "Threads"      "$THREADS"
+
+# Switches (ohne '1' anhängen!)
+add_switch "AllowFpsUpsample" "$ALLOW_FPS_UPSAMPLE"
+add_switch "MergeAudio"       "$MERGE_AUDIO"
+add_switch "NormalizeAudio"   "$NORMALIZE_AUDIO"
+add_switch "MapSubtitles"     "$MAP_SUBTITLES"
+add_switch "Overwrite"        "$OVERWRITE"
+add_switch "DryRun"           "$DRYRUN"
+
+# Debug
+echo "PARAMS: $PARAMS"
+sleep 10
 
 # Run PowerShell script with built params
 while true; do
